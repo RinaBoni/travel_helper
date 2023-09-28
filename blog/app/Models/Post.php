58ad50a022744a5a9fe6590da830e24e9e0684ac
RinaBoni <brisovae@mail.ru>
2023-09-29 06:09:25 +0900

    Удаление постов, используем soft delete

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -4,14 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     //делаем явную привязку таблиц
     protected $table = 'posts';
-    //для возможности изменять таблицу 
+    //для возможности изменять таблицу
     protected $guarded = false;
 
     public function tags(){
