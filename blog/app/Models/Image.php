58a343c0de9bace658e26037a12b37f2569b5d96
RinaBoni <brisovae@mail.ru>
2024-05-11 15:54:40 +0900

    провела миграцию по созданию таблицы images

--- blog/app/Models/Image.php
+++ blog/app/Models/Image.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     use HasFactory;
+    //делаем явную привязку таблиц
+    protected $table = 'images';
+    //для возможности изменять таблицу
+    protected $guarded = false;
+
 }
